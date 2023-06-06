e54f3d2925b082a71e5af04fbcf44fff0152f4fb
Gustavo Gamboa <gustavo.gamboa@allianzdirect.es>
2023-06-06 16:34:58 +0200

    incluida vista inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './view/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrincipalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
